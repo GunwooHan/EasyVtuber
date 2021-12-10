c20ccb9be886b44d6c25e37bddbff885d209292b
GunwooHan <gunwoohan@naver.com>
2021-12-10 17:41:35 +0900

    ADD utils.py comment

--- utils.py
+++ utils.py
@@ -2,10 +2,41 @@ import torch
 import numpy as np
 
 
+def linear_rgb2srgb(image):
+    """
+    convert linear rgb image to srgb image
+    Args:
+        image (numpy array): linear rgb color space image
+
+    Returns:
+        image (numpy array): srgb color space image
+    """
+    return np.where(image <= 0.04045, image / 12.92, ((image + 0.055) / 1.055) ** 2.4)
+
+
+def srgb2linear_rgb(image):
+    """
+    convert srgb image to linear rgb image
+    Args:
+        image (numpy array): srgb color space image
+
+    Returns:
+        image (numpy array): linear rgb color space image
+    """
+    return np.where(image <= 0.003130804953560372, image * 12.92, 1.055 * (image ** (1.0 / 2.4)) - 0.055)
+
+
 def preprocessing_image(image):
+    """
+    convert image(numpy array, unit8) to tensor
+    Args:
+        image (numpy array): character image (256x256, alpha channel included)
+    Returns:
+        tensor
+    """
     np_image = np.array(image) / 255
     clipped_image = np.clip(np_image, 0, 1)
-    srgb_image = np.where(clipped_image <= 0.04045, clipped_image / 12.92, ((clipped_image + 0.055) / 1.055) ** 2.4)
+    srgb_image = linear_rgb2srgb(clipped_image)
     h, w, c = srgb_image.shape
     linear_image = srgb_image.reshape(h * w, c)
     for pixel in linear_image:
@@ -16,19 +47,35 @@ def preprocessing_image(image):
     return torch_image
 
 
-def postprocessing_image(image):
-    image = image.detach().squeeze(0)
-    reshaped_image = image.permute(1, 2, 0)
-    np_image = reshaped_image.numpy()
+def postprocessing_image(tensor):
+    """
+    convert tensor to image(numpy array, unit8)
+    Args:
+        tensor
+    Returns:
+        image (numpy array): character image (256x256, alpha channel included)
+    """
+    tensor = tensor.detach().squeeze(0)
+    reshaped_tensor = tensor.permute(1, 2, 0)
+    np_image = reshaped_tensor.numpy()
     np_image = (np_image + 1) / 2
     srgb_image = np_image[..., :3]
     alpha_image = np_image[..., 3]
     clipped_image = np.clip(srgb_image, 0, 1)
-    rgb_image = np.where(clipped_image <= 0.003130804953560372, clipped_image * 12.92, 1.055 * (clipped_image ** (1.0 / 2.4)) - 0.055)
+    rgb_image = srgb2linear_rgb(clipped_image)
     rgba_image = np.concatenate([rgb_image, alpha_image[..., np.newaxis]], axis=2)
     rgba_image = rgba_image * 255
     return rgba_image.astype(np.uint8)
 
 
 def get_distance(a, b):
+    """
+    calculate euclidean distance a to b
+    Args:
+        a (landmark): 3d points
+        b (landmark): 3d points
+
+    Returns:
+        L2 distance (float)
+    """
     return np.sqrt((a.x - b.x) ** 2 + (a.y - b.y) ** 2)
