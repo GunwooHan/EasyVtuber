0eb49d31e203404fa5696e11b77fd69343b71752
yuyuyzl <yuzhongliang221@gmail.com>
2022-05-04 19:58:26 +0800

    bats

--- 01.构建运行环境（国内源）.bat
+++ 01.构建运行环境（国内源）.bat
@@ -1,6 +1,6 @@
 python --version
 python -m venv venv
 call venv\Scripts\activate.bat
-pip install -r .\requirements.txt
+pip install -r .\requirements.txt -i https://pypi.tuna.tsinghua.edu.cn/simple
 pip install torch --extra-index-url https://download.pytorch.org/whl/cu113
 pause
