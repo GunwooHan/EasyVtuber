9e94dc4d93cc8250786c316e46a5aeb2adbb466e
yuzhongliang22 <yuzhongliang221@gmail.com>
2022-07-31 17:38:03 +0800

    Revert "Fix the problem that Python cannot find modules"

--- 01B.构建运行环境（国内源）.bat
+++ 01B.构建运行环境（国内源）.bat
@@ -3,5 +3,4 @@ python -m venv venv
 call venv\Scripts\activate.bat
 pip install -r .\requirements.txt -i https://pypi.tuna.tsinghua.edu.cn/simple
 pip install torch --extra-index-url https://download.pytorch.org/whl/cu113
-type .\venv\Lib\site-packages\__init__.py
 pause
