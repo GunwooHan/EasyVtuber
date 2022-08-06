a0cbbdd12a7c4b2b7b53ceabf894f7b54fd59fc2
yuyuyzl <yuzhongliang221@gmail.com>
2022-08-06 20:23:06 +0800

    convert rgba

--- 02A.启动器.bat
+++ 02A.启动器.bat
@@ -4,6 +4,6 @@ call venv\Scripts\activate.bat
 if defined VIRTUAL_ENV (
 start pythonw launcher.py
 ) else (
-echo Python venv cannot be activated. (Missing .\venv\Scripts\activate.bat)
+echo Missing .\venv\Scripts\activate.bat, Python venv cannot be activated. 
 echo Please check the project folder and your python installation. Only english character is allowed in their paths.
 )
--- 02B.启动器（调试输出）.bat
+++ 02B.启动器（调试输出）.bat
@@ -4,7 +4,7 @@ call venv\Scripts\activate.bat
 if defined VIRTUAL_ENV (
 start python launcher.py
 ) else (
-echo Python venv cannot be activated. (Missing .\venv\Scripts\activate.bat)
+echo Missing .\venv\Scripts\activate.bat, Python venv cannot be activated. 
 echo Please check the project folder and your python installation. Only english character is allowed in their paths.
 )
 pause
