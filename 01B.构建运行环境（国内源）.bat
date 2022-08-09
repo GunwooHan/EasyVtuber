796215f5b58df8dbcc10765313fef6d7d4c90aac
yuyuyzl <yuzhongliang221@gmail.com>
2022-08-09 20:05:31 +0800

    include venv

--- 01B.构建运行环境（国内源）.bat
+++ 01B.构建运行环境（国内源）.bat
@@ -1,6 +1,6 @@
 @echo off
 cd bin
 python -m pip install wheel -i https://mirror.sjtu.edu.cn/pypi/web/simple
-python -m pip install -r .\requirements.txt -i https://mirror.sjtu.edu.cn/pypi/web/simple
+python -m pip install -r ..\requirements.txt -i https://mirror.sjtu.edu.cn/pypi/web/simple
 python -m pip install torch --extra-index-url https://mirror.sjtu.edu.cn/pytorch-wheels/cu113
 pause
