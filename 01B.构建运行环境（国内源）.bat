a8bf5f3b57102eb02cf3d07c62ea32f842eb947d
yuyuyzl <yuzhongliang221@gmail.com>
2022-08-09 19:39:40 +0800

    include venv

--- 01B.构建运行环境（国内源）.bat
+++ 01B.构建运行环境（国内源）.bat
@@ -1,15 +1,12 @@
 @echo off
-python --version
-echo Initializing Python Virtual Environment (.\venv)...
-python -m venv venv
 echo Activating Python Virtual Environment...
-call venv\Scripts\activate.bat
+call bin\Scripts\activate.bat
 if defined VIRTUAL_ENV (
 pip install wheel -i https://mirror.sjtu.edu.cn/pypi/web/simple
 pip install -r .\requirements.txt -i https://mirror.sjtu.edu.cn/pypi/web/simple
 pip install torch --extra-index-url https://mirror.sjtu.edu.cn/pytorch-wheels/cu113
 ) else (
-echo Missing .\venv\Scripts\activate.bat, Python venv cannot be activated. 
-echo Please check the project folder and your python installation. Only english character is allowed in their paths.
+echo Missing .\bin\Scripts\activate.bat, Python venv cannot be activated.
+echo Please check the project folder. Only english character is allowed in their paths.
 )
 pause
