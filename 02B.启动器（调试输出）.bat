35ec343514b62ed6b8ff23c5c00f127ae7e519cc
yuyuyzl <yuzhongliang221@gmail.com>
2022-08-09 22:04:33 +0800

    tk

--- 02B.启动器（调试输出）.bat
+++ 02B.启动器（调试输出）.bat
@@ -1,10 +1,3 @@
 @echo off
-echo Activating Python Virtual Environment...
-call bin\Scripts\activate.bat
-if defined VIRTUAL_ENV (
-start python launcher.py
-) else (
-echo Missing .\bin\Scripts\activate.bat, Python venv cannot be activated.
-echo Please check the project folder. Only english character is allowed in their paths.
-)
+bin\python.exe launcher.py
 pause
