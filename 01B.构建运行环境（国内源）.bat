75f9202df8af678586859cb7db541f7f6ac13164
yuyuyzl <yuzhongliang221@gmail.com>
2023-01-07 14:44:44 +0800

    osf

--- 01B.构建运行环境（国内源）.bat
+++ 01B.构建运行环境（国内源）.bat
@@ -1,5 +1,5 @@
 @echo off
 cd bin
 python.exe -m pip install -r ..\requirements.txt -i https://mirror.sjtu.edu.cn/pypi/web/simple --no-warn-script-location
-python.exe -m pip install torch --extra-index-url https://mirror.sjtu.edu.cn/pytorch-wheels/cu116 --no-warn-script-location
+python.exe -m pip install torch --extra-index-url https://download.pytorch.org/whl/cu116 --no-warn-script-location
 pause
