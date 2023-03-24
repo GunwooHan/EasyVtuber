eff22139815f854a3c472151deaddccff7f8a398
yuzhongliang22 <yuzhongliang221@gmail.com>
2023-03-24 17:26:15 +0800

    Update 01A.构建运行环境（默认源）.bat

--- 01A.构建运行环境（默认源）.bat
+++ 01A.构建运行环境（默认源）.bat
@@ -1,5 +1,5 @@
 @echo off
 cd bin
 python.exe -m pip install -r ..\requirements.txt --no-warn-script-location
-python.exe -m pip install torch --extra-index-url https://download.pytorch.org/whl/cu116 --no-warn-script-location
+python.exe -m pip install torch==1.13.1+cu116 --extra-index-url https://download.pytorch.org/whl/cu116 --no-warn-script-location
 pause
